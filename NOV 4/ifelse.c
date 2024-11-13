a8e72bac495a73a217424295504ce638cb724ffa
Pavanram-r <pr463698@gmail.com>
2024-11-13 12:30:04 +0530

    Add files via upload

---
+++ NOV 4/ifelse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a=25,b=31,c=44;
+    if(a>b){
+        if(a>c){
+        
+        printf("TRUE");
+    }
+    else{
+        printf("FALSE");
+    }
+    }
+    else{
+        printf("INVALID");
+    }
+    return 0;
+}
